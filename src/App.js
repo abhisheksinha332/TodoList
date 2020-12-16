756f0a44192620fb9e0754e2b9c01013991b41c1
Abhishek Sinha <3327abhishek@gmail.com>
2020-12-17 00:03:18 +0530

    react basic framework and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+//importing components
+import Form from "./components/form";
+import TodoList from "./components/todolist";
+
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos ,setTodos] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <header>
+       <h1>Abhishek's Todo list</h1>
+     </header>
+     <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+     <TodoList />
     </div>
   );
 }
